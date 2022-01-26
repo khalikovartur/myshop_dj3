e214ef45d2df60107c01c76d75321bd168ea5045
artur <khalikart@gmail.com>
2022-01-26 11:30:45 +0500

    fixed mistakes

--- cart/cart.py
+++ cart/cart.py
@@ -11,6 +11,24 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
         
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products
+        from the database.
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+            
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+             
+        
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -31,23 +49,7 @@ class Cart(object):
             del self.cart[product_id]
             self.save()
             
-    def __iter__(self):
-        """
-        Iterate over the items in the cart and get the products
-        from the database.
-        """
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)][product] = product
-            
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
-        
+   
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
     
